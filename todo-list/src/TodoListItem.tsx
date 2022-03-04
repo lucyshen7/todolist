e90fb727caf2ac216a46d180c49bc2d9d085f42b
Lucy Shen <lucyshen77@gmail.com>
2022-03-04 10:10:58 -0800

    create a todo list item

---
+++ todo-list/src/TodoListItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+
+interface Props {
+  todo: Todo;
+}
+
+// the TodoListItem component will take a todo item as a prop. The todo item has a text & boolean property.
+
+export const TodoListItem: React.FC<Props> = ( {todo} ) => { // Declare TodoListItem as a functional component (React.FC) and pass Props as a generic
+  return (
+    <li>
+      <label 
+        style={{ textDecoration: todo.complete ? 'line-through' : undefined }}
+      >
+        <input type="checkbox" checked={todo.complete} /> {todo.text}
+      </label>
+    </li>
+  );
+}
